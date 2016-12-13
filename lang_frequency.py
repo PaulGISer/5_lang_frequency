523a8a399d2004a0e850206a08574b0d61203216
PaulGISer <paul.dolgi@yandex.ru>
2016-12-13 12:10:15 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,18 +1,18 @@
 # _*_ coding: cp866 _*_
 import string,sys
 if sys.argv[1]=="--help":
-	print("""Эта программа выводит 10 наиболее часто встречающихся слов в текстовом файле.
+    print("""Эта программа выводит 10 наиболее часто встречающихся слов в текстовом файле.
 В качестве параметра командной строки необходимо передать путь к файлу""")
 else:
-	strp=string.whitespace+string.punctuation+string.digits+"\"'"+'\u2013'
-	f=open(sys.argv[1],'r')
-	dic={}
-	for line in f:
-	    for word in line.lower().split():
-	        word=word.strip(strp)
-	        if word:
-	            dic[word]=dic.get(word,0)+1
-	f.close()
-	print("Итак, 10 наиболее популярных слов в файле ",sys.argv[1],':')
-	for i in sorted(list(dic.items()),key=lambda x: x[1],reverse=True)[:10]:
-	    print (i[0]+' '+str(i[1]))
+    strp=string.whitespace+string.punctuation+string.digits+"\"'"+'\u2013'
+    f=open(sys.argv[1],'r')
+    dic={}
+    for line in f:
+        for word in line.lower().split():
+            word=word.strip(strp)
+            if word:
+                dic[word]=dic.get(word,0)+1
+    f.close()
+    print("Итак, 10 наиболее популярных слов в файле ",sys.argv[1],':')
+    for i in sorted(list(dic.items()),key=lambda x: x[1],reverse=True)[:10]:
+        print (i[0]+' '+str(i[1]))
