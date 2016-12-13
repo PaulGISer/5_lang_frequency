073480f8d4c037029fcfa79547958699b74ad92e
PaulGISer <paul.dolgi@yandex.ru>
2016-12-13 11:26:50 +0300

    Update lang_frequency.py

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,14 @@
+# _*_ coding: cp866 _*_
+import string,sys
+strp=string.whitespace+string.punctuation+string.digits+"\"'"+'\u2013'
+f=open(sys.argv[1],'r')
+dic={}
+for line in f:
+    for word in line.lower().split():
+        word=word.strip(strp)
+        if word:
+            dic[word]=dic.get(word,0)+1
+f.close()
 
-
-def load_data(filepath):
-    pass
-
-
-def get_most_frequent_words(text):
-    pass
-
-
-if __name__ == '__main__':
-    pass
+for i in sorted(list(dic.items()),key=lambda x: x[1],reverse=True)[:10]:
+    print (i[0]+' '+str(i[1]))
